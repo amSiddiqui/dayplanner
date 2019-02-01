96870bf7eae55ef726c6215e1bd60c75561a32c7
Aamir <gt_ams@yahoo.in>
2019-02-01 23:46:57 +0530

    config database

--- app.js
+++ app.js
@@ -9,25 +9,14 @@ const indexRoutes = require("./routes/index");
 
 // Setting up database
 const db = require("./modules/database");
-const connection = new db();
+
 // Configuring app
 app.use(bodyParser.urlencoded({extended: false}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
 
-// Create connection 
-connection.connect((err)=>{
-    if (err){
-        console.error("Error connecting: "+err.stack);
-        return;
-    }
-
-    console.log("Connected to database as id "+connection.threadId);
-});
-
 // Starting routes
 app.use(indexRoutes);
-
 
 // Application listening
 app.listen(process.env.PORT, ()=>{
--- modules/database.js
+++ modules/database.js
@@ -1,14 +1,10 @@
 const mysql = require("mysql");
-var db = null;
 
-module.exports = function(){
-    if(!db){
-        db = mysql.createConnection({
-            host: process.env.HOST,
-            user: process.env.DBUSER,
-            password: process.env.DBPASS,
-            database: process.env.DBNAME
-        });
-    }
-    return db;
-};
+var pool = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DBNAME
+});
+
+module.exports = pool;
