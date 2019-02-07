bd95dd7c9330d6dfb9ed18109cda7cbdcd20ba85
Aamir <gt_ams@yahoo.in>
2019-02-07 22:49:36 +0530

    add index for schedule

--- routes/index.js
+++ routes/index.js
@@ -15,36 +15,37 @@ router.get("/schedule/", (req, res)=>{
             res.render("dbError");
             throw err;
         }
-        console.log("The results are", results[0]);
+        res.render("Schedule/index", {schedules: results});
     });
 });
 
 
 // TODO: Create route to display a specific Schedule.
 router.get("/schedule/:id/", (req, res)=>{
-
+    res.send("This will show the Schedule with id: "+req.params.id);
 });
 
 // TODO: Create route to create a Schedule.
 router.get("/schedule/new", (req, res)=>{
-
+    res.send("This route will show form to create a new Schedule");
 });
 
 router.post("/schedule/", (req, res)=>{
-
+    res.send("This route will create a new Schedule");
 });
 
 // TODO: Create a route to edit a specific Schedule.
 router.get("/schedule/:id/edit", (req, res)=>{
-
+    res.send("This route will show from to edit the schedule with id "+req.params.id);
 });
 
 router.put("/schedule/:id", (req, res)=>{
-
+    res.send("This route will update the Schedule with id "+req.params.id);
 });
+
 // TODO: Create a route to delete a specific Schedule.
 router.delete("/schedule/:id", (req, res)=>{
-
+    res.send("This route will delete the Schedule with the id "+req.params.id);
 });
 
 
